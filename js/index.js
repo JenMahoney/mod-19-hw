20b11247e32d6cd54d0b062eb76f05f4da8f9225
JenMahoney <164809654+JenMahoney@users.noreply.github.com>
2024-08-19 19:19:06 -0500

    added preloader

--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
-$(document).ready(function() {
-    $(window).on('scroll', function() {
-        $('.workGridArea').each(function() {
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+function fade() {
+    $(".preloader").fadeOut("slow");
 
-            if (bottom_of_window > bottom_of_element) {
-                $(this).animate({
-                    opacity: 1,
-                    transform: "translateY(0)"
-                }, 1000);
-            }
-        });
-    });
-});
+  }
+
+setTimeout(fade, 3000);
